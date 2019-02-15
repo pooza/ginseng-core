3b861a3614a42afff12f85f02dcd7cd4f4474943
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-02-15 13:27:58 +0900

    #42

---
+++ test/sinatra_test.rb
@@ -0,0 +1,28 @@
+require 'rack/test'
+
+module Ginseng
+  class SinatraTest < Test::Unit::TestCase
+    include ::Rack::Test::Methods
+
+    def setup
+      @config = Config.instance
+    end
+
+    def app
+      return Sinatra
+    end
+
+    def test_about
+      header 'User-Agent', Package.user_agent
+      get '/about'
+      assert(last_response.ok?)
+      assert_equal(%("#{Package.name} #{Package.version}"), last_response.body)
+    end
+
+    def test_not_found
+      header 'User-Agent', Package.user_agent
+      get '/not_found'
+      assert_false(last_response.ok?)
+    end
+  end
+end
