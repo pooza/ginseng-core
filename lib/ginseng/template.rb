cbd17b4ae89067c8a33b3dfe6a666333e822bbf3
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-03-04 09:42:17 +0900

    #55

--- lib/ginseng/template.rb
+++ lib/ginseng/template.rb
@@ -3,9 +3,10 @@ require 'erb'
 module Ginseng
   class Template
     include Package
+    attr_reader :params
 
     def initialize(name)
-      @path = File.join(environment_class.constantize.dir, 'views', "#{name}.erb")
+      @path = File.join(environment_class.constantize.dir, dir, "#{name}.erb")
       @erb = ERB.new(File.read(@path), nil, '-')
       @params = {}.with_indifferent_access
     end
@@ -19,8 +20,18 @@ module Ginseng
       @params[key] = value
     end
 
+    def params=(params)
+      @params = params.with_indifferent_access
+    end
+
     def to_s
       return @erb.result(binding)
+    end
+
+    private
+
+    def dir
+      return 'views'
     end
   end
 end
