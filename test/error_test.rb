dc02129aabe5b1e3aa3a4be3218616491b0f8293
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-03-23 07:24:19 +0900

    #61

--- test/error_test.rb
+++ test/error_test.rb
@@ -10,6 +10,10 @@ module Ginseng
       assert(@error.is_a?(Error))
     end
 
+    def test_backtrace_level
+      assert(@error.backtrace_level.is_a?(Integer))
+    end
+
     def test_status
       assert_equal(@error.status, 500)
     end
