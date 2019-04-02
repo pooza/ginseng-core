0e0b2441791d4921a42bbc3b70ce8b845c9889af
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-04-03 07:16:04 +0900

    #66

---
+++ test/string_test.rb
@@ -0,0 +1,8 @@
+module Ginseng
+  class StringTest < Test::Unit::TestCase
+    def test_ellipsize!
+      assert_equal('キュアソード'.ellipsize(5), 'キュアソー…')
+      assert_equal('hogehogehoge'.ellipsize!(4), 'hoge…')
+    end
+  end
+end
