ea04d38128fd01c0144db05368219a81a49c93ae
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-07-06 11:12:25 +0900

    #96

--- lib/ginseng/environment.rb
+++ lib/ginseng/environment.rb
@@ -1,4 +1,5 @@
 require 'socket'
+require 'time'
 
 module Ginseng
   class Environment
@@ -47,6 +48,10 @@ module Ginseng
     def self.gem_fresh?
       Dir.chdir(dir)
       return `git status`.include?('Gemfile.lock') == false
+    end
+
+    def self.tz
+      return Time.now.strftime('%:z')
     end
 
     def self.uid
