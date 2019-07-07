502d318533e1fd1779f27d5b26ea080115aaa87e
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-07-07 14:34:05 +0900

    #99

--- lib/task/test.rb
+++ lib/task/test.rb
@@ -1,11 +1,8 @@
-namespace :ginseng do
-  namespace :core do
-    task :test do
-      ENV['TEST'] = Ginseng::Package.name
-      require 'test/unit'
-      Dir.glob(File.join(Ginseng::Environment.dir, 'test/*')).each do |t|
-        require t
-      end
-    end
+desc 'test all'
+task :test do
+  ENV['TEST'] = Ginseng::Package.name
+  require 'test/unit'
+  Dir.glob(File.join(Ginseng::Environment.dir, 'test/*')).each do |t|
+    require t
   end
 end
