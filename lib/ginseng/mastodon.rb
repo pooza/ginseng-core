ab7f8eceea5a7e061a729f869b3a11735ed7c0f2
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-10-19 17:15:55 +0900

    #123

--- lib/ginseng/mastodon.rb
+++ lib/ginseng/mastodon.rb
@@ -89,14 +89,42 @@ module Ginseng
       return @http.get(uri, {headers: headers})
     end
 
-    def self.create_tag(word)
-      return '#' + word.strip.gsub(/[^[:alnum:]]+/, '_').sub(/^_/, '').sub(/_$/, '')
+    def follow(id, params = {})
+      headers = params[:headers] || {}
+      headers['Authorization'] ||= "Bearer #{@token}"
+      headers['X-Mulukhiya'] = package_class.full_name unless mulukhiya_enable?
+      return @http.post(create_uri("/api/v1/accounts/#{id}/follow"), {
+        body: '{}',
+        headers: headers,
+      })
+    end
+
+    def unfollow(id, params = {})
+      headers = params[:headers] || {}
+      headers['Authorization'] ||= "Bearer #{@token}"
+      headers['X-Mulukhiya'] = package_class.full_name unless mulukhiya_enable?
+      return @http.post(create_uri("/api/v1/accounts/#{id}/unfollow"), {
+        body: '{}',
+        headers: headers,
+      })
     end
 
     def create_uri(href = '/api/v1/statuses')
-      uri = @uri.clone
+      uri = self.uri.clone
       uri.path = href
       return uri
+    end
+
+    def create_streaming_uri(stream = 'user')
+      uri = self.uri.clone
+      uri.scheme = 'wss'
+      uri.path = '/api/v1/streaming'
+      uri.query_values = {'access_token' => token, 'stream' => stream}
+      return uri
+    end
+
+    def self.create_tag(word)
+      return '#' + word.strip.gsub(/[^[:alnum:]]+/, '_').sub(/^_/, '').sub(/_$/, '')
     end
   end
 end
