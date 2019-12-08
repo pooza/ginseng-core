8feffafe73a0f7e40d23b692f336eaae911a390a
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-12-08 19:51:37 +0900

    #131

--- lib/ginseng.rb
+++ lib/ginseng.rb
@@ -1,56 +1,22 @@
 require 'active_support'
 require 'active_support/core_ext'
-require 'active_support/dependencies/autoload'
 require 'ginseng/monkey_patches'
 require 'yajl'
 require 'yajl/json_gem'
-
-ActiveSupport::Inflector.inflections do |inflect|
-  inflect.acronym 'CSS'
-  inflect.acronym 'DSN'
-  inflect.acronym 'JSON'
-  inflect.acronym 'HTML'
-  inflect.acronym 'HTTP'
-  inflect.acronym 'URI'
-  inflect.acronym 'URL'
-  inflect.acronym 'XML'
-end
+require 'zeitwerk'
 
 module Ginseng
-  extend ActiveSupport::Autoload
-
-  autoload :CommandLine
-  autoload :Config
-  autoload :Daemon
-  autoload :Environment
-  autoload :Error
-  autoload :HTTP
-  autoload :Lib
-  autoload :Logger
-  autoload :Mastodon
-  autoload :Package
-  autoload :Slack
-  autoload :TagContainer
-  autoload :Template
-  autoload :URI
-
-  autoload_under 'error' do
-    autoload :ConfigError
-    autoload :DatabaseError
-    autoload :GatewayError
-    autoload :ImplementError
-    autoload :NotFoundError
-    autoload :RedisError
-    autoload :RenderError
-    autoload :RequestError
-    autoload :ValidateError
+  def self.dir
+    return File.expand_path('..', __dir__)
   end
 
-  autoload_under 'service' do
-    autoload :YouTubeService
-  end
-
-  autoload_under 'uri' do
-    autoload :VideoURI
+  def self.loader
+    config = YAML.load_file(File.join(dir, 'config/autoload.yaml'))
+    loader = Zeitwerk::Loader.new
+    loader.inflector.inflect(config['inflections'])
+    loader.push_dir(File.join(dir, 'lib'))
+    loader.setup
   end
 end
+
+Ginseng.loader
