e3ae5c3c3e8155b8c535699ba9e43c7d159aa2c8
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-12-09 04:00:44 +0900

    #134

--- lib/ginseng.rb
+++ lib/ginseng.rb
@@ -1,22 +1,56 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/dependencies/autoload'
 require 'ginseng/monkey_patches'
 require 'yajl'
 require 'yajl/json_gem'
-require 'zeitwerk'
 
-module Ginseng
-  def self.dir
-    return File.expand_path('..', __dir__)
-  end
-
-  def self.loader
-    config = YAML.load_file(File.join(dir, 'config/autoload.yaml'))
-    loader = Zeitwerk::Loader.for_gem
-    loader.inflector.inflect(config['inflections'])
-    loader.push_dir(File.join(dir, 'lib'))
-    loader.setup
-  end
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.acronym 'CSS'
+  inflect.acronym 'DSN'
+  inflect.acronym 'JSON'
+  inflect.acronym 'HTML'
+  inflect.acronym 'HTTP'
+  inflect.acronym 'URI'
+  inflect.acronym 'URL'
+  inflect.acronym 'XML'
 end
 
-Ginseng.loader
+module Ginseng
+  extend ActiveSupport::Autoload
+
+  autoload :CommandLine
+  autoload :Config
+  autoload :Daemon
+  autoload :Environment
+  autoload :Error
+  autoload :HTTP
+  autoload :Lib
+  autoload :Logger
+  autoload :Mastodon
+  autoload :Package
+  autoload :Slack
+  autoload :TagContainer
+  autoload :Template
+  autoload :URI
+
+  autoload_under 'error' do
+    autoload :ConfigError
+    autoload :DatabaseError
+    autoload :GatewayError
+    autoload :ImplementError
+    autoload :NotFoundError
+    autoload :RedisError
+    autoload :RenderError
+    autoload :RequestError
+    autoload :ValidateError
+  end
+
+  autoload_under 'service' do
+    autoload :YouTubeService
+  end
+
+  autoload_under 'uri' do
+    autoload :VideoURI
+  end
+end
