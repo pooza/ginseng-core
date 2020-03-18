fbaf7066472c244bd37cec2580a5f3ead7f904c9
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-03-19 06:23:43 +0900

    #158

--- test/hash_test.rb
+++ test/hash_test.rb
@@ -4,11 +4,20 @@ module Ginseng
       @hash = {a: 1, b: {c: 2, d: 3}}
     end
 
-    def test_commaize
+    def test_deep_merge
       assert_equal(@hash.deep_merge(b: {d: 4}, e: 5), {a: 1, b: {c: 2, d: 4}, e: 5})
 
-      @hash.deep_merge!(b: {d: 4}, e: 5, f: 10)
-      assert_equal(@hash, {a: 1, b: {c: 2, d: 4}, e: 5, f: 10})
+      tmp = @hash.clone
+      tmp.deep_merge!(b: {d: 4}, e: 5, f: 10)
+      assert_equal(tmp, {a: 1, b: {c: 2, d: 4}, e: 5, f: 10})
+    end
+
+    def test_key_flatten
+      assert_equal(@hash.key_flatten, {'/a' => 1, '/b/c' => 2, '/b/d' => 3})
+
+      tmp = @hash.clone
+      tmp.key_flatten!
+      assert_equal(tmp, {'/a' => 1, '/b/c' => 2, '/b/d' => 3})
     end
   end
 end
