fe8d6f6826ba1479073934869327827e18934246
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-05-22 18:59:54 +0900

    #195

--- test/string_test.rb
+++ test/string_test.rb
@@ -1,8 +1,16 @@
 module Ginseng
   class StringTest < Test::Unit::TestCase
-    def test_ellipsize!
+    def test_ellipsize
       assert_equal('キュアソード'.ellipsize(5), 'キュアソー…')
       assert_equal('hogehogehoge'.ellipsize!(4), 'hoge…')
+    end
+
+    def test_nfkc
+      assert_equal('！！！！！!!!!'.nfkc, '!!!!!!!!!')
+    end
+
+    def test_sanitize
+      assert_equal('<p>なぎさ&ほのか</p>'.sanitize, 'なぎさ&ほのか ')
     end
   end
 end
